d03ae179209c92e658e0d0c17fd97eeddbd7ddcb
alvinbank1 <61926776+alvinbank1@users.noreply.github.com>
2024-01-11 20:24:16 +0900

    Add files via upload

---
+++ Run Internet Explorer/Run Internet Explorer/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Run_Internet_Explorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("https://alvinbank1.github.io/InternetExplorer/");
+        }
+
+        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
